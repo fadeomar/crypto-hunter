ef071d076c4905efd2f084f6dc64fce7a10a7a0a
fadi <fadeomar2015@gmail.com>
2022-10-01 20:54:56 +0300

    setup the codebase

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Header from './components/Header'; 
+import Home from './pages/Home'; 
+import Coin from './pages/Coin'; 
+
+import * as S from './style';
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <S.AppContainer>
+    <Header />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/coins/:id" element={<Coin />} />
+    </Routes>
+    </S.AppContainer>
+  </BrowserRouter>
   );
 }
 
